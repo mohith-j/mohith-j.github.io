75661478c9dbd1f7fc7ae4663d02ba8638ff10a4
Mohith Jothi Kannan <jothiteenu@gmail.com>
2023-02-22 22:55:48 -0700

    added links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,16 +5,10 @@ function navbar(){
         <nav className=" py-10 mb-12 flex justify-between">
             <h1 className="font-burtons text-3xl">My Portfolio</h1>
             <ul className="flex items-center text-teal-600">
-              {/* <li>
-                <BsFillMoonStarsFill
-                  onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl"
-                />
-              </li> */}
               <li className="mx-5">
                 <a
                   className="bg-gradient-to-t from-cyan-500 to-teal-500 bg-cyan-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
+                  href="https://drive.google.com/file/d/1RM4lyC40QOHuSNH4I45SI4QeneTSVrBh/view?usp=sharing" target="_blank"
                 >
                   Resume
                 </a>
--- src/components/socialmedia.js
+++ src/components/socialmedia.js
@@ -4,9 +4,9 @@ import { AiFillGithub, AiFillLinkedin, AiFillMail} from "react-icons/ai";
 function socialmedia(){
     return(
         <div className="text-5xl flex justify-center gap-16 text-gray-700">
-            <AiFillGithub />
-            <AiFillLinkedin />
-            <AiFillMail />
+            <a href="https://github.com/mohith-j" target="_blank"><AiFillGithub /></a> 
+            <a href="https://www.linkedin.com/in/mohithj/" target="_blank"><AiFillLinkedin /></a>
+            <a href="mailto:mjothika@asu.edu" target="_blank"><AiFillMail /></a>
         </div>
     )
 };
